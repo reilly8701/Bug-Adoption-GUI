a6dd867f43d34fd6c5ea83bf444d0d688f54ea14
reilly8701 <74623774+reilly8701@users.noreply.github.com>
2020-12-09 21:28:31 -0600

    Add project files.

---
+++ Bug Adoption/Game.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bug_Adoption
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game : Page
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Uri page = new Uri("mantis.xaml", UriKind.Relative);
+            NavigationService.Navigate(page);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Uri page = new Uri("stag beetle.xaml", UriKind.Relative);
+            NavigationService.Navigate(page);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Uri page = new Uri("ant.xaml", UriKind.Relative);
+            NavigationService.Navigate(page);
+        }
+    }
+}
---
+++ Bug Adoption/stag beetle.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bug_Adoption
+{
+    /// <summary>
+    /// Interaction logic for stag_beetle.xaml
+    /// </summary>
+    public partial class stag_beetle : Page
+    {
+        public stag_beetle()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
